b61c7848bae16fa8f3fd3d9c93cdde311c8630a4
Isabelle Girard <idg.011008@gmail.com>
2025-11-12 11:30:57 -0300

    create commit

---
+++ Trabalho Programacao/FrmEntrada.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Trabalho_Programacao
+{
+    partial class FrmEntrada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.senhacadastro = new System.Windows.Forms.TextBox();
+            this.emailcadastro = new System.Windows.Forms.TextBox();
+            this.nome = new System.Windows.Forms.TextBox();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.senhalogin = new System.Windows.Forms.TextBox();
+            this.emaillogin = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.senhacadastro);
+            this.groupBox1.Controls.Add(this.emailcadastro);
+            this.groupBox1.Controls.Add(this.nome);
+            this.groupBox1.Controls.Add(this.usuario);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 426);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnsair);
+            this.groupBox3.Controls.Add(this.btnlimpar);
+            this.groupBox3.Controls.Add(this.btncadastrar);
+            this.groupBox3.Location = new System.Drawing.Point(91, 198);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 181);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(44, 124);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(112, 23);
+            this.btnsair.TabIndex = 2;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(44, 87);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(112, 23);
+            this.btnlimpar.TabIndex = 1;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.Location = new System.Drawing.Point(46, 43);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(112, 23);
+            this.btncadastrar.TabIndex = 0;
+            this.btncadastrar.Text = "Cadastrar";
+            this.btncadastrar.UseVisualStyleBackColor = true;
+            this.btncadastrar.Click += new System.EventHandler(this.btncadastrar_Click);
+            // 
+            // senhacadastro
+            // 
+            this.senhacadastro.Location = new System.Drawing.Point(144, 152);
+            this.senhacadastro.Name = "senhacadastro";
+            this.senhacadastro.Size = new System.Drawing.Size(100, 20);
+            this.senhacadastro.TabIndex = 19;
+            // 
+            // emailcadastro
+            // 
+            this.emailcadastro.Location = new System.Drawing.Point(144, 118);
+            this.emailcadastro.Name = "emailcadastro";
+            this.emailcadastro.Size = new System.Drawing.Size(100, 20);
+            this.emailcadastro.TabIndex = 18;
+            // 
+            // nome
+            // 
+            this.nome.Location = new System.Drawing.Point(191, 81);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(100, 20);
+            this.nome.TabIndex = 17;
+            // 
+            // usuario
+            // 
+            this.usuario.Location = new System.Drawing.Point(152, 48);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(100, 20);
+            this.usuario.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Senha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(103, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "E-mail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Nome Completo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Usuario";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.btnlogin);
+            this.groupBox2.Controls.Add(this.senhalogin);
+            this.groupBox2.Controls.Add(this.emaillogin);
+            this.groupBox2.Location = new System.Drawing.Point(400, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(388, 426);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Login";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(98, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Senha";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(98, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "E-mail";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(136, 198);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(112, 23);
+            this.btnlogin.TabIndex = 22;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // senhalogin
+            // 
+            this.senhalogin.Location = new System.Drawing.Point(148, 104);
+            this.senhalogin.Name = "senhalogin";
+            this.senhalogin.Size = new System.Drawing.Size(100, 20);
+            this.senhalogin.TabIndex = 21;
+            // 
+            // emaillogin
+            // 
+            this.emaillogin.Location = new System.Drawing.Point(148, 70);
+            this.emaillogin.Name = "emaillogin";
+            this.emaillogin.Size = new System.Drawing.Size(100, 20);
+            this.emaillogin.TabIndex = 20;
+            // 
+            // FrmEntrada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmEntrada";
+            this.Text = "FrmEntrada";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.TextBox senhacadastro;
+        private System.Windows.Forms.TextBox emailcadastro;
+        private System.Windows.Forms.TextBox nome;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.TextBox senhalogin;
+        private System.Windows.Forms.TextBox emaillogin;
+    }
+}
---
+++ Trabalho Programacao/FrmPerfil.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Trabalho_Programacao
+{
+    partial class FrmPerfil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPerfil));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.minhaAreaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.perfilUSUARIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.criarListaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minhasListasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SugestãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.livrosUSUARIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.filmesUSUARIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome Completo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E-mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Senha";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(198, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(237, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(190, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(190, 156);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(402, 415);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(112, 23);
+            this.btnsair.TabIndex = 2;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            // 
+            // btneditar
+            // 
+            this.btneditar.Location = new System.Drawing.Point(272, 415);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(112, 23);
+            this.btneditar.TabIndex = 8;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.minhaAreaToolStripMenuItem,
+            this.SugestãoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(126, 450);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // minhaAreaToolStripMenuItem
+            // 
+            this.minhaAreaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.perfilUSUARIOToolStripMenuItem,
+            this.criarListaToolStripMenuItem,
+            this.minhasListasToolStripMenuItem});
+            this.minhaAreaToolStripMenuItem.Name = "minhaAreaToolStripMenuItem";
+            this.minhaAreaToolStripMenuItem.Size = new System.Drawing.Size(113, 19);
+            this.minhaAreaToolStripMenuItem.Text = "Minha Area";
+            // 
+            // perfilUSUARIOToolStripMenuItem
+            // 
+            this.perfilUSUARIOToolStripMenuItem.Name = "perfilUSUARIOToolStripMenuItem";
+            this.perfilUSUARIOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.perfilUSUARIOToolStripMenuItem.Text = "Perfil";
+            this.perfilUSUARIOToolStripMenuItem.Click += new System.EventHandler(this.perfilUSUARIOToolStripMenuItem_Click);
+            // 
+            // criarListaToolStripMenuItem
+            // 
+            this.criarListaToolStripMenuItem.Name = "criarListaToolStripMenuItem";
+            this.criarListaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.criarListaToolStripMenuItem.Text = "Criar Lista";
+            this.criarListaToolStripMenuItem.Click += new System.EventHandler(this.criarListaToolStripMenuItem_Click);
+            // 
+            // minhasListasToolStripMenuItem
+            // 
+            this.minhasListasToolStripMenuItem.Name = "minhasListasToolStripMenuItem";
+            this.minhasListasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.minhasListasToolStripMenuItem.Text = "Minhas Listas";
+            this.minhasListasToolStripMenuItem.Click += new System.EventHandler(this.minhasListasToolStripMenuItem_Click);
+            // 
+            // SugestãoToolStripMenuItem
+            // 
+            this.SugestãoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.livrosUSUARIOToolStripMenuItem,
+            this.filmesUSUARIOToolStripMenuItem});
+            this.SugestãoToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("SugestãoToolStripMenuItem.Image")));
+            this.SugestãoToolStripMenuItem.Name = "SugestãoToolStripMenuItem";
+            this.SugestãoToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.SugestãoToolStripMenuItem.Text = "Sugestão";
+            // 
+            // livrosUSUARIOToolStripMenuItem
+            // 
+            this.livrosUSUARIOToolStripMenuItem.Name = "livrosUSUARIOToolStripMenuItem";
+            this.livrosUSUARIOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.livrosUSUARIOToolStripMenuItem.Text = "Livros";
+            this.livrosUSUARIOToolStripMenuItem.Click += new System.EventHandler(this.livrosUSUARIOToolStripMenuItem_Click);
+            // 
+            // filmesUSUARIOToolStripMenuItem
+            // 
+            this.filmesUSUARIOToolStripMenuItem.Name = "filmesUSUARIOToolStripMenuItem";
+            this.filmesUSUARIOToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.filmesUSUARIOToolStripMenuItem.Text = "Filmes";
+            this.filmesUSUARIOToolStripMenuItem.Click += new System.EventHandler(this.filmesUSUARIOToolStripMenuItem_Click);
+            // 
+            // FrmPerfil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmPerfil";
+            this.Text = "Pefil";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.FrmCliente_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem minhaAreaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem perfilUSUARIOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem criarListaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem minhasListasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SugestãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem livrosUSUARIOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem filmesUSUARIOToolStripMenuItem;
+    }
+}
